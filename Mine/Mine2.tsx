44ef2e77ba0c0ee721ec2bce11b0446b7b770aa4
nick-santos <66573166+nick-santos@users.noreply.github.com>
2026-02-13 00:42:22 -0300

    Rename main folder to "source"
    - Delete "Mine" folder, as it was replaced by source\map\ folder.

--- Mine/Mine2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Mine" tilewidth="8" tileheight="8" tilecount="162" columns="27">
- <image source="Mine/Mine.png" width="217" height="55"/>
-</tileset>
--- Mine/MineWB.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="MineWB" tilewidth="8" tileheight="8" spacing="1" margin="1" tilecount="144" columns="24">
- <image source="Mine/MineWB.png" width="217" height="55"/>
-</tileset>
